b633800dae2f221fd4466f494af8367faa4a55e4
Jalilov <jalilovjavohir777@gmail.com>
2021-04-28 23:16:14 +0500

    finished imshow_data. dataset plots

--- NumPy/imshow_data.py
+++ NumPy/imshow_data.py
@@ -1,4 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from mnist import get_data
+filename = 'mnist_test.csv'
 
-def imshow_data(label,dataset):
-    pass
+def imshow_data(label:np.ndarray,dataset:np.ndarray)->None:
+    plt.figure(figsize=(7,10))
+    for i in range(1,10):
+        plt.subplot(3,3,i)
+        plt.title(f'label = {label[i]}',pad=10)
+        plt.imshow(dataset[i],cmap='gray')
+        plt.axis('off')
+    plt.show()
+
+label,dataset = get_data(filename)
+imshow_data(label,dataset)
