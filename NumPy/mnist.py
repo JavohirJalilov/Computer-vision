2fbae87d9468a6b3a07ef342d661ef4980146bce
Jalilov <jalilovjavohir777@gmail.com>
2021-04-28 18:14:11 +0500

    create def get_data function

--- NumPy/mnist.py
+++ NumPy/mnist.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 filename = 'mnist_test.csv'
-data = np.genfromtxt(filename,delimiter=',')
-print(data[0])
+def get_data(filename):
+    data = np.genfromtxt(filename,delimiter=',',dtype=np.int64)
+    print(data[0])
+
+get_data(filename)
