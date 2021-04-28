0519ee58449eed08542fa129834b223ccd7b54b8
Jalilov <jalilovjavohir777@gmail.com>
2021-04-28 18:15:01 +0500

    function add doc string

--- NumPy/mnist.py
+++ NumPy/mnist.py
@@ -2,6 +2,14 @@ import numpy as np
 
 filename = 'mnist_test.csv'
 def get_data(filename):
+    '''
+    Edit the data using the numpy genfromtxt() function.
+  
+    Parametrs:
+        str: filename
+    returns:
+        Numpy array: array
+  '''
     data = np.genfromtxt(filename,delimiter=',',dtype=np.int64)
     print(data[0])
 
