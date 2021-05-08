e35fe6e2bde2df4fc32fecc568b6dfb6ecb8402e
Jalilov <jalilovjavohir777@gmail.com>
2021-05-08 15:06:20 +0500

    object detection finish

--- Computer Vision/bbox/main.py
+++ Computer Vision/bbox/main.py
@@ -2,14 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-points_arr = np.genfromtxt('bbox_road.csv',delimiter=',',dtype=np.str_)
-points = (points_arr[:,:1],points_arr[:,1:5])
-
 def drawing(img,points):
     labels,points = points
-    print(labels)
-    print(points)
+    for i,item in enumerate(points):
+        (x,y),(w,h) = item.reshape((2,2))
+        start_point = (x,y)
+        end_point = (x+w,y+h)
+        label = labels[i][0]
 
+        cv2.rectangle(img,start_point,end_point,color=(255, 0, 0),thickness=3)
+        cv2.putText(img,label,(start_point[0]-15,start_point[1]-15),cv2.FONT_HERSHEY_SIMPLEX,2.0,color=(255, 0, 0),thickness=3)
+        plt.imshow(img)
+    plt.show()
+
+points_arr = np.genfromtxt('bbox_road.csv',delimiter=',',dtype=np.str_)
+points = (points_arr[:,:1],np.array(points_arr[:,1:5],dtype=np.uint16))
 img = cv2.imread('crossing.jpg')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
 drawing(img,points)
