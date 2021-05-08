64d07cbc692a700a7d90c166a4d862166e9aec47
Jalilov <jalilovjavohir777@gmail.com>
2021-05-08 15:46:31 +0500

    The image object detection function is terminated

--- Computer Vision/bbox/drawing_bbox_label.py
+++ Computer Vision/bbox/drawing_bbox_label.py
@@ -2,7 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-def drawing_bbox(img,points):
+
+def drawing_bbox(img,filename_csv):
+    '''
+    Image object detection.
+    file CSV, Points format: COCO
+    Parameters:
+        img: numpy.array.
+        filename_csv: str.
+    '''
+    points_arr = np.genfromtxt(filename_csv,delimiter=',',dtype=np.str_)
+    points = (points_arr[:,:1],np.array(points_arr[:,1:5],dtype=np.uint16))
     labels,points = points
     for i,item in enumerate(points):
         (x,y),(w,h) = item.reshape((2,2))
