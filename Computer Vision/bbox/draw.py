4611ce12549d7b71a5d4d578c6092d1e61015499
Jalilov <jalilovjavohir777@gmail.com>
2021-05-08 15:47:02 +0500

    visualization img

--- Computer Vision/bbox/draw.py
+++ Computer Vision/bbox/draw.py
@@ -3,9 +3,8 @@ import numpy as np
 import cv2
 import drawing_bbox_label
 
-points_arr = np.genfromtxt('bbox_road.csv',delimiter=',',dtype=np.str_)
-points = (points_arr[:,:1],np.array(points_arr[:,1:5],dtype=np.uint16))
+filename = 'bbox_road.csv'
 img = cv2.imread('crossing.jpg')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-drawing_bbox_label.drawing_bbox(img,points)
+drawing_bbox_label.drawing_bbox(img,filename)
